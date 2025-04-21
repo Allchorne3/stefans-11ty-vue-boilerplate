cca406f96fefa2d1444af184314996c32e50a75a
Stefan Allchorne <allchorne39@sky.com>
2025-04-21 23:10:40 +0100

    Initial commit

---
+++ site/src/scripts/app.js
@@ -0,0 +1,6 @@
+const moment = require("moment") // For date formatting
+
+document.addEventListener('DOMContentLoaded', () => {
+	// Get the current yeaer
+	document.getElementById('is-year').innerHTML = moment().year();
+})
