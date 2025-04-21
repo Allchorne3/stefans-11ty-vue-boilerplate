d2152a3992beec815f4029e09221a1bd6ab6e549
Stefan Allchorne <allchorne39@sky.com>
2025-04-21 23:30:44 +0100

    Add vue 3 with example component

--- site/src/scripts/app.js
+++ site/src/scripts/app.js
@@ -1,6 +1,15 @@
 const moment = require("moment") // For date formatting
 
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
 document.addEventListener('DOMContentLoaded', () => {
 	// Get the current yeaer
 	document.getElementById('is-year').innerHTML = moment().year();
+
+	createApp({
+	  components: {
+		ExampleComponent
+	  }
+	}).mount('#app');
 })
